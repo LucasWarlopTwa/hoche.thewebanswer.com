d05f8b9c87d4059c6fe82e1df9731119b8eacbe3
Lucas W <work@lucas-warlop.com>
2022-03-08 13:59:05 +0100

    Update DayType.php

--- src/Form/DayType.php
+++ src/Form/DayType.php
@@ -3,14 +3,20 @@
 namespace App\Form;
 
 use App\Entity\Day;
+use App\Entity\Dinner;
 use App\Entity\Lunch;
 use App\Repository\LunchRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
 
 class DayType extends AbstractType
 {
@@ -19,9 +25,34 @@ class DayType extends AbstractType
         $builder
             ->add('dateOfService', DateType::class, [
                 'widget' => 'single_text',
+                'label' => "Date",
+                'required' => true,
+                'constraints' => new NotBlank(),
+                'constraints' => new NotNull(),
+                'constraints' => new Length(['min' => 3, 'max' => 30]),
+                'attr' => [
+                    'placeholder' => "Date",
+                ],
             ])
-            ->add('lunchOfTheDay')
-            ->add('dinnerOfTheDay')
+            ->add('lunchOfTheDay', EntityType::class, [
+                'class' => Lunch::class,
+                'label' => "Déjeuner",
+                'required' => true,
+                'constraints' => new NotBlank(),
+                'constraints' => new NotNull(),
+                'constraints' => new Length(['min' => 3, 'max' => 30])
+                ]
+            )
+            ->add('dinnerOfTheDay', EntityType::class, [
+                'class' => Dinner::class,
+                'label' => "Diner",
+                'required' => true,
+                'constraints' => new NotBlank(),
+                'constraints' => new NotNull(),
+                'constraints' => new Length(['min' => 3, 'max' => 30])
+                ]
+            )
+
         ;
     }
 
